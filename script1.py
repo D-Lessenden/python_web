024808d5efa00a16271c4f36127c9747eeb62a7d
Daniel Lessenden <dless27@gmail.com>
2021-01-02 19:34:46 -0600

    Add a render template for different pages

--- script1.py
+++ script1.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Website content goes here!"
+    return render_template("home.html")
+    # return "Website content goes here!"
 
 @app.route('/about/')
 def about():
-    return "Website about content goes here!"
+    return render_template("about.html")
+
+    # return "Website about content goes here!"
 
 if __name__=="__main__":
     app.run(debug=True)
